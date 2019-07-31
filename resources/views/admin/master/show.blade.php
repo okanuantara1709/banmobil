f72914d6966cfe0a6e1094febb1a1d70ed772428
vanessaangle <com.widyaop@gmail.com>
2019-08-01 07:51:51 +0800

    Done anjg

--- resources/views/admin/master/show.blade.php
+++ resources/views/admin/master/show.blade.php
@@ -63,6 +63,12 @@
                                                     {{AppHelper::viewRelation($data,$item['view_relation'])}}
                                                 </td>
                                             </tr>
+                                            @elseif(array_key_exists('format',$item) && !empty($item['format']))
+                                                @if ($item['format'] == 'rupiah')
+                                                    <td>{{$item['label']}}</td>
+                                                    <td>:</td>
+                                                    <td>Rp. {!! number_format($data->{$item['name']},2,',','.') !!}</td>
+                                                @endif
                                             @else
                                             <tr>
                                                 <td>{{$item['label']}}</td>
